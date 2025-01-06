1c506471dab3d4c7de4d614cb683416e7f67391f
oldding666 <1101623201@qq.com>
2025-01-06 22:37:42 +0800

    [a.fu] #014 feature: use setter method to DI

--- spring-01-ioc/src/main/java/com/afuya/study/spring01ioc/repository/UserDao.java
+++ spring-01-ioc/src/main/java/com/afuya/study/spring01ioc/repository/UserDao.java
@@ -2,6 +2,7 @@ package com.afuya.study.spring01ioc.repository;
 
 import com.afuya.study.spring01ioc.bean.Dog;
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -16,7 +17,13 @@ public class UserDao {
     Dog dog;
 
     // Spring更推荐使用构造器注入
-    public UserDao(Dog dog) {
+//    public UserDao(Dog dog) {
+//        this.dog = dog;
+//    }
+
+    // setter方法注入
+    @Autowired
+    public void setDog(Dog dog) {
         this.dog = dog;
     }
 }
